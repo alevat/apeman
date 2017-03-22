695041f95d72433d4dcf7d7ebab7ece1c1083bfe
Eric Tavela <etavela@5amsolutions.com>
2017-03-21 20:47:01 -0400

    Added Spring Boot configuration

--- src/main/java/com/alevat/apeman/ApemanApplication.java
+++ src/main/java/com/alevat/apeman/ApemanApplication.java
@@ -1,4 +1,13 @@
 package com.alevat.apeman;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ApemanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApemanApplication.class, args);
+    }
+    
 }
