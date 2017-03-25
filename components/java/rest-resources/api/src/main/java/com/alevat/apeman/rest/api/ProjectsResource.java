135cb8f7bb62370cdbcd7e90eaa5c7ceedc6ecba
Eric Tavela <etavela@5amsolutions.com>
2017-03-25 04:40:30 -0400

    Implemented stubbed REST API

--- components/java/rest-resources/api/src/main/java/com/alevat/apeman/rest/api/ProjectsResource.java
+++ components/java/rest-resources/api/src/main/java/com/alevat/apeman/rest/api/ProjectsResource.java
@@ -1,8 +1,15 @@
 package com.alevat.apeman.rest.api;
 
+import com.alevat.apeman.api.dto.Project;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping
+import java.util.List;
+
+@RequestMapping("/api/v1/projects")
 public interface ProjectsResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    List<Project> getProjects();
 
 }
---
+++ components/java/shared-api/src/main/java/com/alevat/apeman/api/dto/Project.java
@@ -0,0 +1,15 @@
+package com.alevat.apeman.api.dto;
+
+public class Project {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
