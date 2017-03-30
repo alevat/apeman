8c7a9c83da2b93ed487ab1868cf545d427c4cab4
Eric Tavela <etavela@5amsolutions.com>
2017-03-30 07:30:03 -0400

    Fixed code

--- components/java/rest-resources/implementation/src/main/java/com/alevat/apeman/rest/controllers/ProjectsResourceRestController.java
+++ components/java/rest-resources/implementation/src/main/java/com/alevat/apeman/rest/controllers/ProjectsResourceRestController.java
@@ -6,14 +6,12 @@ import com.google.common.collect.Lists;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Random;
 
 @RestController
 public class ProjectsResourceRestController implements ProjectsResource {
 
     @Override
     public List<Project> getProjects() {
-        new Random().nextInt();
         Project project = new Project();
         project.setName("Hello World");
         return Lists.newArrayList(project);
