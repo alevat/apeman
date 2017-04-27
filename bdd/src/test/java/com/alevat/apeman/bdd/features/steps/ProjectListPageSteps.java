04e4054ac5839797bdd0f508d0cd7968dd27484f
Eric Tavela <etavela@5amsolutions.com>
2017-04-27 16:00:19 -0400

    Added Spring Context injection to BDD and added data to existing test

--- bdd/src/test/java/com/alevat/apeman/bdd/features/steps/ProjectListPageSteps.java
+++ bdd/src/test/java/com/alevat/apeman/bdd/features/steps/ProjectListPageSteps.java
@@ -2,6 +2,10 @@ package com.alevat.apeman.bdd.features.steps;
 
 import com.alevat.apeman.bdd.pages.ApemanHomePage;
 import com.alevat.apeman.bdd.tasks.SelectMenuItem;
+import com.alevat.apeman.bdd.util.SpringUtil;
+import com.alevat.apeman.entities.Project;
+import com.alevat.apeman.repositories.ProjectRepository;
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -9,6 +13,10 @@ import cucumber.api.java.en.When;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
+import org.springframework.context.ApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
@@ -17,10 +25,19 @@ public class ProjectListPageSteps {
 
     private ApemanHomePage theHomePage;
     private ProjectsAreListed projectsAreListed = new ProjectsAreListed();
+    private ProjectRepository projectRepository;
+    private List<Project> createdProjects = new ArrayList<>();
 
     @Before
     public void set_the_stage() {
         OnStage.setTheStage(new OnlineCast());
+        ApplicationContext context = SpringUtil.getContext();
+        projectRepository = context.getBean(ProjectRepository.class);
+    }
+
+    @After
+    public void clean_up() {
+        projectRepository.delete(createdProjects);
     }
 
     @Given("^that a (.*) is logged in$")
@@ -30,7 +47,10 @@ public class ProjectListPageSteps {
 
     @Given("^that Projects exist in the system$")
     public void that_Projects_exist_in_the_system() throws Throwable {
-        // no-op for now; project list is stubbed on server side
+        Project project = new Project();
+        project.setName("name_" + System.currentTimeMillis());
+        project = projectRepository.save(project);
+        createdProjects.add(project);
     }
 
     @When("^a (.*) selects (.*) from the Menu$")
